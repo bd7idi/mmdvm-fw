8683f1dcc6073c01afd586542acaa3a2793f458e
bd7idi <55025589+bd7idi@users.noreply.github.com>
2024-07-03 22:45:30 +0800

    Update update_sam.sh

--- update_sam.sh
+++ update_sam.sh
@@ -6,7 +6,7 @@ wget https://github.com/bd7idi/mmdvm-fw/blob/master/mmdvm_f1_sam.bin
 # DV-Mega Mods
 sudo systemctl stop mmdvmhost.timer
 sudo systemctl stop mmdvmhost.service
-sudo sudo stm32flash -v -w mmdvm_f1_sam.bin -g 0x0 -R -i 20,-21,21:-20,21 /dev/ttyAMA0
+sudo sudo stm32flash -v -w mmdvm_f1_sam.bin -g 0x0 -R -i 23,-22,22:-23,22 /dev/ttyAMA0 
 sudo systemctl start mmdvmhost.timer
 sudo systemctl restart mmdvmhost.service
 sudo rm mmdvm_f1_sam.bin
