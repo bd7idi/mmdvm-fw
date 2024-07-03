b89378840e5c83c5fcb94a786562f71c56d8f708
bd7idi <55025589+bd7idi@users.noreply.github.com>
2024-07-03 22:45:09 +0800

    Update update_dual.sh

--- update_dual.sh
+++ update_dual.sh
@@ -6,7 +6,7 @@ wget https://github.com/bd7idi/mmdvm-fw/blob/master/mmdvm_f1_dual.bin
 # DV-Mega Mods
 sudo systemctl stop mmdvmhost.timer
 sudo systemctl stop mmdvmhost.service
-sudo stm32flash -v -w mmdvm_f1_dual.bin -g 0x0 -R -i 20,-21,21:-20,21 /dev/ttyAMA0
+sudo stm32flash -v -w mmdvm_f1_dual.bin -g 0x0 -R -i 23,-22,22:-23,22 /dev/ttyAMA0 
 sudo systemctl start mmdvmhost.timer
 sudo systemctl restart mmdvmhost.service
 sudo rm mmdvm_f1_dual.bin
